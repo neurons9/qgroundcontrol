14b9c566f0b4185b86200f862312458dc810614c
Jacob Dahl <dahl.jakejacob@gmail.com>
2025-06-09 21:25:46 -0800

    updates

--- src/API/QGCCorePlugin.cc
+++ src/API/QGCCorePlugin.cc
@@ -316,7 +316,6 @@ const QVariantList &QGCCorePlugin::toolBarIndicators()
     static const QVariantList toolBarIndicatorList = QVariantList(
         {
             QVariant::fromValue(QUrl::fromUserInput(QStringLiteral("qrc:/qml/QGroundControl/Toolbar/RTKGPSIndicator.qml"))),
-            QVariant::fromValue(QUrl::fromUserInput(QStringLiteral("qrc:/qml/QGroundControl/Controls/EscIndicator.qml"))),
         }
     );
 
